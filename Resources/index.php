511015b0a467e26e9487d0e2f418bff8b5388917
lbdagnachew <101952961+lbdagnachew@users.noreply.github.com>
2022-04-10 19:57:21 -0500

    Create index.php

---
+++ Resources/index.php
@@ -0,0 +1,5 @@
+![Outcomes_vs_Goals](https://user-images.githubusercontent.com/101952961/162645699-0be5f6e8-ce24-42f3-8df6-b1127fcb572f.png)
+
+![Theater_Outcomes_vs_Launch](https://user-images.githubusercontent.com/101952961/162645717-6266762d-249c-49b2-8a52-9065b715922b.png)
+
+
